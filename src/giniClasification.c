d266060e9edada9d3389234a54eedb2a71dcdc6b
KathM1 <katherinemeza144@gmail.com>
2026-06-23 10:16:56 -0600

    Add Gini classification function

---
+++ src/giniClasification.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "clasificacion.h"
+
+void clasificar_gini(float gini) {
+    printf("Clasificacion de la distribución del ingreso: ");
+
+    if (gini < 0.30) {
+        printf("Muy buena distribucion del ingreso\n");
+    } else if (gini < 0.35) {
+        printf("Buena distribucion del ingreso\n");
+    } else if (gini < 0.40) {
+        printf("Distribucion regular del ingreso\n");
+    } else if (gini < 0.45) {
+        printf("Distribucion desigual del ingreso\n");
+    } else if (gini < 0.50) {
+        printf("Mala distribucion del ingreso\n");
+    } else {
+        printf("Enorme desigualdad del ingreso\n");
+    }
+}
