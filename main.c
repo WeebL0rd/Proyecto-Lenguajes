1001d782699def0abb67d759c78a525204cd5716
RachellLeiva <r.leiva.1@estudiantec.cr>
2026-06-25 00:01:46 -0600

    main

--- main.c
+++ main.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
+#include "include/entradaDatos.h"
 #include "include/indexCalculation.h"
+#include "include/giniClasification.h"
 
-int main(void) {
-    //Test del cálculo de la fórmula de Gini
-    float quintilesUS[] = {0.031, .113, 0.252, 0.478, 1};
-    float gini = calculoGini(quintilesUS, 5);
-    printf("Indice: %f\n", gini);
+int main(void)
+{
+    char pais[TAM_NOMBRE_PAIS];
+    float quintiles[CANTIDAD_QUINTILES];
+    float gini;
+    int continuar;
+
+    printf("==============================================\n");
+    printf("   Calculadora del Indice de Gini\n");
+    printf("   Proyecto Lenguajes de Programacion\n");
+    printf("==============================================\n\n");
+
+    do {
+        if (!leerDatosPais(pais, TAM_NOMBRE_PAIS, quintiles)) {
+            break;
+        }
+
+        gini = calculoGini(quintiles, CANTIDAD_QUINTILES);
+
+        printf("\n----------------------------------------------\n");
+        printf("Pais: %s\n", pais);
+        printf("Indice de Gini: %.4f\n", gini);
+        clasificarGini(gini);
+        printf("----------------------------------------------\n");
+
+        continuar = preguntarContinuar();
+
+    } while (continuar);
+
+    printf("\nSe cerrará la calculadora del Indice de Gini.\n");
+
     return 0;
 }
